652b2ebf00117995bf3f1e1eafb385c7633e3b80
thatonovember <thatonovember@github.com>
2020-10-10 11:48:59 +0200

    Implementing InMemory Repository

--- OnlineWebStore/OnlineWebStore.WebUI/Controllers/ProductManagerController.cs
+++ OnlineWebStore/OnlineWebStore.WebUI/Controllers/ProductManagerController.cs
@@ -11,14 +11,17 @@ namespace OnlineWebStore.WebUI.Controllers
 {
     public class ProductManagerController : Controller
     {
-        ProductRepository context;
-        ProductCategoryRepository productCategories;
+        
+        InMemoryRepository<Product> context;
+        InMemoryRepository<ProductCategory> productCategories;
+
+     
 
 
         public ProductManagerController()
         {
-            context = new ProductRepository();
-            productCategories = new ProductCategoryRepository();
+            context = new InMemoryRepository<Product>();
+            productCategories = new InMemoryRepository<ProductCategory>();
         }
         // GET: ProductManager
         public ActionResult Index()
