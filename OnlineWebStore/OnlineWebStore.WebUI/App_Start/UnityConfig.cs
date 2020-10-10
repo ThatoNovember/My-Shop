adf761d5461f5e0366c70489910061315c7b4811
thatonovember <thatonovember@github.com>
2020-10-10 14:23:26 +0200

    SQLREPO

--- OnlineWebStore/OnlineWebStore.WebUI/App_Start/UnityConfig.cs
+++ OnlineWebStore/OnlineWebStore.WebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using OnlineWebStore.Core.Contracts;
 using OnlineWebStore.Core.Models;
 using OnlineWebStore.DataAccess.InMemory;
+using OnlineWebStore.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace OnlineWebStore.WebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }
