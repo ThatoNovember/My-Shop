b073e768cc6c4e4a531d878cac1909c2f2ee2049
thatonovember <thatonovember@github.com>
2020-10-10 13:22:43 +0200

    EntityImplementation

---
+++ OnlineWebStore/OnlineWebStore.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using OnlineWebStore.Core.Models;
+
+namespace OnlineWebStore.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Product>Pproducts { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
