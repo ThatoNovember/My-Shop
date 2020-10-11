d534e229eae78d6110aaefcefda360b6ffb86e95
thatonovember <thatonovember@github.com>
2020-10-11 22:21:11 +0200

    implementing product listing

--- OnlineWebStore/OnlineWebStore.WebUI/Controllers/HomeController.cs
+++ OnlineWebStore/OnlineWebStore.WebUI/Controllers/HomeController.cs
@@ -1,18 +1,41 @@
-﻿using System;
+﻿using OnlineWebStore.Core.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using OnlineWebStore.Core.Models;
 namespace OnlineWebStore.WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
         {
-            return View();
+            context = productContext;
+            productCategories = productCategoryContext;
         }
 
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
